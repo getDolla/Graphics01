6399eb1341746794854f23c6bb1ef5c9f0eda652
getDolla <ywang8@stuy.edu>
2017-02-04 15:47:56 -0500

    done

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ def draw(filename, x, y):
     while r < y:
         c = 0
         while c < x:
-            R = (c + r)%256
-	    G = (c - r)%256
-	    B = (r/2) % 256
+            R = (c + r)%256 #red value
+	    G = (c - r)%256 #green value
+	    B = (r/2) % 256 #blue value
 
             s += str(R) + " " + str(G) + " " + str(B) + " \n"
             c += 1
         r += 1
 
     with open(filename+".ppm", "w") as f:
-        f.write(s)
+        f.write(s) #write to ppm file
 
 draw("pic", 500, 500)
 
