5e47e06273cb0b6e60db8c1eb9b47667c529d879
getDolla <ywang8@stuy.edu>
2017-02-04 15:26:19 -0500

    done?

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def draw(filename, x, y):
     while r < y:
         c = 0
         while c < x:
-            R = (c + r)%255
-	    G = 255 - (r/2)%255
-	    B = (r/2) % 255
+            R = (c + r)%256
+	    G = 255 - (r/2)%256
+	    B = (r/2) % 256
 
             s += str(R) + " " + str(G) + " " + str(B) + " \n"
             c += 1
